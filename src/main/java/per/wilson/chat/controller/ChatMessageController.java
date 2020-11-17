ca84c89f04287223d0f75e51e077f73d0318c8ea
Wilson <Extern.Weixuan.He@kuka.com>
2020-11-17 17:19:40 +0800

    add api

--- src/main/java/per/wilson/chat/controller/ChatMessageController.java
+++ src/main/java/per/wilson/chat/controller/ChatMessageController.java
@@ -1,10 +1,8 @@
 package per.wilson.chat.controller;
 
 import io.springframework.common.response.ServerResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import per.wilson.chat.domain.entity.ChatMessage;
 import per.wilson.chat.service.ChatMessageService;
 
@@ -14,15 +12,27 @@ import java.util.List;
 /**
  * @author Wilson
  */
-@RestController
-@RequestMapping("/chat-message")
+@Controller
+@RequestMapping("/")
 public class ChatMessageController {
     @Resource
     private ChatMessageService chatMessageService;
 
-    @GetMapping("/")
+    @GetMapping("/chat-message")
+    @ResponseBody
     public ServerResponse<List<ChatMessage>> chatMessages(@RequestParam String fromUserId,
                                                           @RequestParam String toUserId) {
         return ServerResponse.success(chatMessageService.chatMessages(fromUserId, toUserId));
+    }
+
+    @GetMapping("/chatroom")
+    public String chatroom() {
+        return "/chatroom";
+    }
+
+    @GetMapping("/index")
+    @ResponseBody
+    public ServerResponse<String> index() {
+        return ServerResponse.success("index");
     }
 }
